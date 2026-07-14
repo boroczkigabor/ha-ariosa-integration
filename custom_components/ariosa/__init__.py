7fe621666e9b0f5120ca9d14db34f8c822cea9dc
boroczkigabor <boroczkigabor@gmail.com>
2026-07-14 09:14:27 +0200

    Initial project structure

---
+++ custom_components/ariosa/__init__.py
@@ -0,0 +1,33 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+type AriosaConfigEntry = ConfigEntry
+
+
+async def async_setup(
+    hass: HomeAssistant,
+    config: dict,
+) -> bool:
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: AriosaConfigEntry,
+) -> bool:
+
+    hass.data.setdefault(DOMAIN, {})
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: AriosaConfigEntry,
+) -> bool:
+
+    hass.data[DOMAIN].pop(entry.entry_id)
+
+    return True
---
+++ custom_components/ariosa/const.py
@@ -0,0 +1,15 @@
+from datetime import timedelta
+
+DOMAIN = "ariosa"
+
+DEFAULT_NAME = "Ariosa Ventilation"
+
+DEFAULT_PORT = 502
+
+DEFAULT_SLAVE = 1
+
+UPDATE_INTERVAL = timedelta(seconds=10)
+
+START_REGISTER = 100
+
+REGISTER_COUNT = 13
