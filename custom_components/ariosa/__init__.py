bd77099ff0f893de8986ca0bd6cf222e3ab47e80
boroczkigabor <boroczkigabor@gmail.com>
2026-07-14 22:34:38 +0200

    Revert back to 2025.1 API

--- custom_components/ariosa/__init__.py
+++ custom_components/ariosa/__init__.py
@@ -1,12 +1,11 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_PORT
 from homeassistant.core import HomeAssistant
-
-from .const import PLATFORMS
+from .const import DOMAIN, PLATFORMS
 from .coordinator import AriosaDataUpdateCoordinator
 from .modbus_client import AriosaClient
 
-type AriosaConfigEntry = ConfigEntry[AriosaDataUpdateCoordinator]
+type AriosaConfigEntry = ConfigEntry
 
 
 async def async_setup(
@@ -27,19 +26,15 @@ async def async_setup_entry(
         port=entry.data[CONF_PORT],
     )
 
-    coordinator = AriosaDataUpdateCoordinator(
+    coord = AriosaDataUpdateCoordinator(
         hass,
         client,
     )
 
-    await coordinator.async_config_entry_first_refresh()
+    await coord.async_config_entry_first_refresh()
 
-    entry.runtime_data = coordinator
-
-    await hass.config_entries.async_forward_entry_setups(
-        entry,
-        PLATFORMS,
-    )
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coord
 
     return True
 
@@ -54,6 +49,7 @@ async def async_unload_entry(
     )
 
     if unload_ok:
-        await entry.runtime_data.client.disconnect()
+        coord = hass.data[DOMAIN].pop(entry.entry_id)
+        await coord.client.disconnect()
 
     return unload_ok
