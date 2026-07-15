13a1a2ddbc57606880dddae5811e3b502d8e7e74
boroczkigabor <boroczkigabor@gmail.com>
2026-07-15 13:53:24 +0200

    Enable custom integrations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,9 @@
+import pytest
+
 pytest_plugins = ["pytest_homeassistant_custom_component.common"]
+
+
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    """Make custom_components/ (e.g. ariosa) loadable in every test's hass fixture."""
+    yield
