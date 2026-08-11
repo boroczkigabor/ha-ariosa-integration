67d92698a2ae5e18e29f126a2a66109c263c2aaa
boroczkigabor <boroczkigabor@gmail.com>
2026-08-11 10:13:50 +0200

    Adjust unit tests

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -57,7 +57,7 @@ async def test_sensors_created_with_correct_state(hass, measurements):
             "sensor.ariosa_ventilation_filter_hours": "50",
             "sensor.ariosa_ventilation_supply_side_heat_recovery_efficiency": "54.1",
             "sensor.ariosa_ventilation_exhaust_side_heat_recovery_efficiency": "63.5",
-            "sensor.ariosa_ventilation_heat_recovery_efficiency_imbalance": "9.4",
+            "sensor.ariosa_ventilation_heat_recovery_efficiency_imbalance": "-9.4",
         }
 
         for entity_id, expected_state in expected_states.items():
